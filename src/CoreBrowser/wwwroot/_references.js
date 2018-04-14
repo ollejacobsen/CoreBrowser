b780872db815fea90faa967e10207ef560807c95
Olle Jacobsen <olle.jacobsen@lundalogik.se>
2018-04-14 21:43:26 +0200

    Removed unused files

--- src/CoreBrowser/wwwroot/_references.js
+++
@@ -1,6 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="js/ekko-lightbox.js" />
-/// <reference path="js/site.min.js" />
-/// <reference path="js/stupidtable.js" />
-/// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
-/// <reference path="lib/jquery/dist/jquery.js" />
